590e3d6442a0861060564c560e91e3f8aad89292
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-09-27 02:00:33 -0300

    5.15. Exercício proposto

--- course/src/course/Program.java
+++ course/src/course/Program.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
 
+		int m = sc.nextInt();
 		int n = sc.nextInt();
-		int[][] mat = new int[n][n];
+		int[][] mat = new int[m][n];
 
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[i].length; j++) {
@@ -16,22 +17,29 @@ public class Program {
 			}
 		}
 
-		System.out.println("Main diagonal:");
-		for (int i = 0; i < mat.length; i++) {
-			System.out.print(mat[i][i] + " ");
-		}
-		System.out.println();
+		int x = sc.nextInt();
 
-		int count = 0;
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[i].length; j++) {
-				if (mat[i][j] < 0) {
-					count++;
+				if (mat[i][j] == x) {
+					System.out.println("Position " + i + "," + j + ":");
+					if (j > 0) {
+						System.out.println("Left: " + mat[i][j - 1]);
+					}
+					if (i > 0) {
+						System.out.println("Up: " + mat[i - 1][j]);
+					}
+					if (j < mat[i].length - 1) {
+						System.out.println("Right: " + mat[i][j + 1]);
+					}
+					if (i < mat.length - 1) {
+						System.out.println("Down: " + mat[i + 1][j]);
+					}
 				}
 			}
 		}
-		System.out.println("Negative numbers = " + count);
 
 		sc.close();
+
 	}
 }
