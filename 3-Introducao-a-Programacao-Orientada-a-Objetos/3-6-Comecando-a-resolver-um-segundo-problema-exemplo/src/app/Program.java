5b9a2e9f447ae8cc7929159af593e9cf42029680
José Malcher Jr <contato@josemalcher.net>
2019-05-03 21:18:49 -0300

    3.8. Finalizando o programa

--- 3-Introducao-a-Programacao-Orientada-a-Objetos/3-6-Comecando-a-resolver-um-segundo-problema-exemplo/src/app/Program.java
+++ 3-Introducao-a-Programacao-Orientada-a-Objetos/3-6-Comecando-a-resolver-um-segundo-problema-exemplo/src/app/Program.java
@@ -11,21 +11,37 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Product product = new Product();
 		System.out.println("Enter product data:");
-		
+
 		System.out.print("Name: ");
 		product.name = sc.nextLine();
-		
+
 		System.out.print("Price: ");
 		product.price = sc.nextDouble();
-		
+
 		System.out.print("Quantity in Stock: ");
 		product.quantity = sc.nextInt();
+
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
 		
-		System.out.println(product);
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
 		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
 		sc.close();
 	}
 
