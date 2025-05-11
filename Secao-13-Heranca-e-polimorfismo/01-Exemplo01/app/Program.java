3666a5273216d3ef132cc8354aa774ed1499f31c
José Malcher Jr. <contato@josemalcher.net>
2025-05-11 20:22:15 -0300

    131 Upcasting e downcasting

--- Secao-13-Heranca-e-polimorfismo/01-Exemplo01/app/Program.java
+++ Secao-13-Heranca-e-polimorfismo/01-Exemplo01/app/Program.java
@@ -1,9 +1,41 @@
 package app;
 
+import entities.Account;
 import entities.BussinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
     public static void main(String[] args) {
+        Account acc  = new Account(1001, "alex", 0.0);
+        BussinessAccount bacc = new BussinessAccount(1002, "maria", 0.0, 500.0);
 
+        //UPCASTING
+        Account acc1 = bacc;
+
+        Account acc2 = new BussinessAccount(1003, "bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "jose", 0.0, 400.0);
+
+        //DOWNCASTING
+
+        //BussinessAccount acc4 = acc2;
+        BussinessAccount acc4 = (BussinessAccount) acc2;
+        acc4.loan(100.0);
+
+        // BussinessAccount acc5 = (BussinessAccount) acc3;
+        /*
+        Exception in thread "main" java.lang.ClassCastException: class entities.SavingsAccount cannot be cast to class entities.BussinessAccount (entities.SavingsAccount and entities.BussinessAccount are in unnamed module of loader 'app')
+	at app.Program.main(Program.java:24)
+        */
+
+        if (acc3 instanceof BussinessAccount) {
+            BussinessAccount acc5 = (BussinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("LOAN");
+        }
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.upadteBalance();
+            System.out.println("Update");
+        }
     }
 }
---
+++ Secao-13-Heranca-e-polimorfismo/01-Exemplo01/entities/SavingsAccount.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class SavingsAccount extends Account{
+    private Double interestRate;
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void upadteBalance() {
+        balance += balance * interestRate;
+    }
+}
+
